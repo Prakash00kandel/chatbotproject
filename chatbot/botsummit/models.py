f2fd15d2df5aafdd6d8b732ba33b21ded15174b2
ramthapa <thaparam78666@gmail.com>
2020-12-16 08:01:12 +0545

    added a reply in chatbot

--- chatbot/botsummit/models.py
+++ chatbot/botsummit/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Messages(models.Model):
+    user_input = models.TextField()
+    reply = models.TextField()
+
+    def __str__(self):
+        return self.user_input
--- chatbot/botsummit/views.py
+++ chatbot/botsummit/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .main import *
-
+from .models import *
 
 # Create your views here.
 
@@ -12,15 +12,23 @@ def index(request):
 # def about(request):
 #     return HttpResponse()
 def ChatbotView(request):
+
+
     if request.method == "POST":
+        print("this is post")
         get_message = request.POST.get('msg', None)
+
         print('get_message', get_message)
 
         reply_messages = chat(get_message)
         print('reply_messages', reply_messages)
 
+        
         context = {
-            'ram': 'ram'
+            'ram': 'ram',
+            'get_message':get_message,
+            'reply_messages':reply_messages,
+
         }
         return render(request, 'botsummit/base.html', context)
 
